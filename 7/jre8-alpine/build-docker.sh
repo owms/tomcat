cfa0825fb47d9739a360024ed8d28657ee220b8e
Matthew Sanders <matts@spaceflightindustrites.com>
2017-08-28 10:04:36 -0600

    Fix build scripts.

--- 7/jre8-alpine/build-docker.sh
+++ 7/jre8-alpine/build-docker.sh
@@ -15,7 +15,7 @@ NAMESPACE=test
 DOCKER_VERSION=`docker --version | cut -f3 | cut -d '.' -f2`
 [ ${DOCKER_VERSION} -lt 12 ] && TAG_FLAG='-f' || TAG_FLAG=''
 
-docker build -t ${IMAGE_NAME} .
+docker build -t ${IMAGE_NAME} 7/jre8-alpine
 docker tag  ${IMAGE_NAME} 639193537090.dkr.ecr.us-east-1.amazonaws.com/${NAMESPACE}/${IMAGE_NAME}:${TAG}
 docker push 639193537090.dkr.ecr.us-east-1.amazonaws.com/${NAMESPACE}/${IMAGE_NAME}:${TAG}
 docker tag  ${IMAGE_NAME} 639193537090.dkr.ecr.us-east-1.amazonaws.com/${NAMESPACE}/${IMAGE_NAME}:latest
